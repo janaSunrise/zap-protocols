e5361c90b10626a59d13512cd7611a064d4c6550
janaSunrise <warriordefenderz@gmail.com>
2022-06-15 00:10:55 +0530

    🔥 Remove unused import.

--- contracts/currency/CurrencyTransfer.sol
+++ contracts/currency/CurrencyTransfer.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "../utils/Constants.sol";
-
 library CurrencyTransfer {
     using SafeERC20 for IERC20;
 
